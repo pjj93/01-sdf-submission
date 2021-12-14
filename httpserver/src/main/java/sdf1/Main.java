6c14772122a609dc61bdb01d4d2bf2f65a7e7ccb
pjj93 <puayjianjun@gmail.com>
2021-12-14 10:59:07 +0800

    Task 3

--- httpserver/src/main/java/sdf1/Main.java
+++ httpserver/src/main/java/sdf1/Main.java
@@ -1,7 +1,44 @@
 package sdf1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi assessment");
+        List<String> docRoots = new ArrayList<String>(); //stores the list of directories as an ArrayList of type String
+        int port = 3000; // default port number
+        
+        // if condition specifies default port and directory if no args passed
+        if (args.length == 0) {
+            //Path path = Paths.get("./static");
+            docRoots.add("./static");
+            // System.out.println("Pathname: " + path);
+        }
+        else {
+            for (int i=0; i<args.length;i++) {
+                //cmdargs.add(args[i]);
+                if (args[i].equals("--port")) {
+                    if (i+1 < args.length) {
+                        port = Integer.parseInt(args[i+1]);
+                    }
+                }
+                if (args[i].equals("--docRoot")) {
+                    if (i+1 < args.length) {
+                        docRoots = Arrays.asList(args[i+1].split(":"));
+                    }
+                }
+            }
+            if (docRoots.isEmpty()) {
+                docRoots.add("./static");
+            }
+        }
+        
+        // test output for Task 3
+        // System.out.println("Port number: " + port);
+        // for (int i = 0; i< docRoots.size(); i++) {
+        //     System.out.println(docRoots.get(i));
+        // }
+        
     }
 }
